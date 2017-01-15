8eb93ae0b847fd4674fb92609fb98fe66cfc5a6a
cruisediary <cruzdiary@gmail.com>
2017-01-15 19:43:08 +0900

    feat(view): IndicatorCell 추가

---
+++ GitHub/Views/UICollectionViews/IndicatorCell.swift
@@ -0,0 +1,20 @@
+//
+//  IndicatorCell.swift
+//  GitHub
+//
+//  Created by CruzDiary on 15/01/2017.
+//  Copyright © 2017 cruz. All rights reserved.
+//
+
+import UIKit
+
+class IndicatorCell: UICollectionViewCell {
+    static let nibName = "IndicatorCell"
+    static let reuseIdentifier = "IndicatorCell"
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
