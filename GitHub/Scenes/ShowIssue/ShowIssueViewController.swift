cf45e9a9d043684befd7bd8bb5394f0a10daea7a
cruisediary <cruzdiary@gmail.com>
2017-01-15 23:02:11 +0900

    feat: ShowIssueViewController 추가 및 스토리보드 segue 연결

---
+++ GitHub/Scenes/ShowIssue/ShowIssueViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ShowIssueViewController.swift
+//  GitHub
+//
+//  Created by CruzDiary on 15/01/2017.
+//  Copyright © 2017 cruz. All rights reserved.
+//
+
+import UIKit
+
+class ShowIssueViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
