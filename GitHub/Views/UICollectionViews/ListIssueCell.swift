099ec7b611924281a238806945b619c7b3fc2dc7
cruisediary <cruzdiary@gmail.com>
2017-01-15 16:52:32 +0900

    feat: ListIssueCell 추가

---
+++ GitHub/Views/UICollectionViews/ListIssueCell.swift
@@ -0,0 +1,32 @@
+//
+//  ListIssueCell.swift
+//  GitHub
+//
+//  Created by CruzDiary on 15/01/2017.
+//  Copyright © 2017 cruz. All rights reserved.
+//
+
+import UIKit
+
+class ListIssueCell: UICollectionViewCell {
+    static let nibName = "ListIssueCell"
+    static let reuseIdentifier = "ListIssueCell"
+    static let height: CGFloat = 64
+
+    @IBOutlet weak var repoLabel: UILabel!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var opendInfoLabel: UILabel!
+    @IBOutlet weak var issueNumberLabel: UILabel!
+    
+    func configure(issue: Issue) {
+        repoLabel.text = issue.url
+        titleLabel.text = issue.title
+        issueNumberLabel.text = "\(issue.number)"
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
