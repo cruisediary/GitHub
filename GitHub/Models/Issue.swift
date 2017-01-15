fc91c66c775177146f520968c664cf01c8301fd3
cruisediary <cruzdiary@gmail.com>
2017-01-15 15:16:31 +0900

    feat: Issue 모델 추가

---
+++ GitHub/Models/Issue.swift
@@ -0,0 +1,27 @@
+//
+//  Issue.swift
+//  GitHub
+//
+//  Created by CruzDiary on 15/01/2017.
+//  Copyright © 2017 cruz. All rights reserved.
+//
+
+import UIKit
+
+import ObjectMapper
+
+struct Issue: Mappable {
+    var url: String?
+    var number: Int?
+    var title: String?
+    var state: String?
+
+    init?(map: Map) {}
+
+    mutating func mapping(map: Map) {
+        url <- map["url"]
+        number <- map["number"]
+        title <- map["title"]
+        state <- map["state"]
+    }
+}
